c20636e6519ff8a01777116e6e148f0bed2961fd
Your Name <you@example.com>
2020-07-12 16:51:12 -0700

    first draft of level 0 module 1 pythong curriculum

---
+++ Level0-Module1-Py/_02_variables_practice/circle_area_calculator.py
@@ -0,0 +1,3 @@
+# Write a Python program that asks the user for the radius 
+# of a circle and displays the area of that circle
+# The formula for the area of a circle is πr^2
---
+++ Level0-Module1-Py/_04_int/_1_riddler/riddler.py
@@ -0,0 +1,12 @@
+'''
+* Write a python program that asks the user a minimum of 3 riddles.
+
+* You can look at riddles.com if you don't already know any riddles.
+
+* Collect the response of each riddle from the user and compare their
+  answers to the correct answer. 
+
+* Use a variable to keep track of the correctly answered riddles
+
+* After all the riddles have been asked, tell the user how many they got correct
+'''
