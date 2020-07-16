c69884d11cfcf48075fbb8cf4a648dee2ea5051b
Daniel Commins <dencee@users.noreply.github.com>
2020-07-16 11:51:04 -0700

    fixed settings

--- Level0-Module1-Py/_02_variables_practice/circle_area_calculator.py
+++ Level0-Module1-Py/_02_variables_practice/circle_area_calculator.py
@@ -7,40 +7,29 @@ import math
 #       The formula for the area of a circle is πr^2.
 #       See example image in package to check your output.
 
-# Create an if-main code block, *hint, type main then ctrl+space to auto-complete
 if __name__ == '__main__':
-    
-    # Make a new window variable, window = Tk()
     window = Tk()
-    
-    # Hide the window using the window's .withdraw() method
     window.withdraw()
     
     # Ask the user for the radius in pixels and store it in a variable
     # simpledialog.askinteger()
-    radius = simpledialog.askinteger(title='', prompt='radius in pixels?')
     
     # Make a new turtle
-    daniel = turtle.Turtle()
     
     # Have your turtle draw a circle with the correct radius
-    daniel.circle(radius=radius, extent=360, steps=30)
+    # my_turtle.circle()
 
     # Call the turtle .penup() method
-    daniel.penup()
 
     # Move your turtle to a new x,y position using .goto()
-    daniel.goto(-50, -50)
 
     # Calculate the area of your circle and store it in a variable, you can use math.pi
-    area = radius*radius*math.pi
     
     # Write the area of your circle using the turtle .write() method
     # myTurtle.write(arg="area = " + str(area), move=True, align='left', font=('Arial',8,'normal'))
-    daniel.write(arg="area = " + str(area), move=False, align='left', font=('Arial',8,'normal'))
 
     # Hide your turtle
-    daniel.hideturtle()
+
+    # Call turtle.done()
 
     window.mainloop()
-    turtle.done()
