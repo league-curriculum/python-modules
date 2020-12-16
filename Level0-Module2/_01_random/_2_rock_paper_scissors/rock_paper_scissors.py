cbe164e4f380592ec0f0450f57a0a48db0ebfd23
Tyler Wiltz <twiltz@ucsd.edu>
2020-12-16 14:18:22 -0800

    Figured out GUI for RockPapersScissors, need to include random choice

---
+++ Level0-Module2/_01_random/_2_rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,30 @@
+from tkinter import Tk, Label, Button
+
+from PIL import ImageTk, Image
+
+def onclick(args):
+    if args == 1:
+        print("rock")
+    if args == 2:
+        print("paper")
+    if args == 3:
+        print("scissors")
+        
+if __name__ == '__main__':
+    window = Tk()
+    window.title("Join")
+    window.geometry("1075x250")
+    window.configure(background="grey")
+
+    Label(window, text="Choose Your Weapon!", bg="grey").pack(side="left")
+
+    img_rock = ImageTk.PhotoImage(file="rock.png")
+    img_paper = ImageTk.PhotoImage(file="paper.jpeg")
+    img_scissors = ImageTk.PhotoImage(file="scissors.jpeg")
+
+    Button(window, image=img_rock, command=lambda:onclick(1)).pack(side="left")
+    Button(window, image=img_paper, command=lambda:onclick(2)).pack(side="left")
+    Button(window, image=img_scissors, command=lambda:onclick(3)).pack(side="left")
+
+    # Start the GUI
+    window.mainloop()
