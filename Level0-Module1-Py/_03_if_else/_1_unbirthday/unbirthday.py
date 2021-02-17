21978536e1ffc33427872adbd1ec1626c6115efa
Daniel Commins <dencee@users.noreply.github.com>
2021-02-16 20:48:05 -0800

    removed Py in solution folder names

--- Level0-Module1-Py/_03_if_else/_1_unbirthday/unbirthday.py
+++
@@ -1,9 +0,0 @@
-from tkinter import messagebox, simpledialog, Tk
-
-if __name__ == '__main__':
-    birthday = simpledialog.askstring(None, "When is your birthday (mm/dd)?")
-
-    if birthday == "08/07":
-        messagebox.showinfo(None, "Happy Birthday!")
-    else:
-        messagebox.showinfo(None, "Happy UN-Birthday!")
--- Level0-Module1-Py/_05_for_loops/_3_bananas_forever/bananas_forever.py
+++
@@ -1,7 +0,0 @@
-'''
-* Write a python program that prints the word 'banana' one thousand (1,000) times
-'''
-if __name__ == '__main__':
-    for i in range(1000):
-        print(str(i) + '. banana')
-
