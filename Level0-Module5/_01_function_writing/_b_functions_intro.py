6e1537c68fe5779dfe56922e38c867ed6654259d
Daniel Commins <dencee@users.noreply.github.com>
2021-05-08 12:31:31 -0700

    improved googly eyes, function tests
    removed secret message box because it's a bad recipe

--- Level0-Module5/_01_function_writing/_b_functions_intro.py
+++ Level0-Module5/_01_function_writing/_b_functions_intro.py
@@ -5,32 +5,49 @@ from tkinter import messagebox, simpledialog, Tk
 import random
 import unittest
 
+# TODO Look at the test methods below and define the functions used in those
+#  tests to make the tests pass. For example, the first test function has the
+#  following code:
+#       self.assertEqual(100, multiply(10, 10))
+#  This code calls a multiply function and passes in 2 values (10 and 10) and
+#  checks the function returns 100. Since a multiply function isn't defined,
+#  you have to define one with the correct input variable(s) and return
+#  statement. Create your functions below and not inside the test class.
 
-# TODO Write your function definitions on the next line below!
 
+# ======================= DO NOT EDIT THE CODE BELOW =========================
 
 class FunctionTests(unittest.TestCase):
 
-    # TODO 1) Write a function definition ABOVE AND OUTSIDE this class for multiply
-    def test_1(self):
+    def test_function_1(self):
         self.assertEqual(100, multiply(10, 10))
 
-    # TODO 2) Write a function definition ABOVE AND OUTSIDE this class for str_cat
-    def test_2(self):
+    def test_function_2(self):
         self.assertEqual('Welcome to Python', str_cat(var1='Welcome', var2='to', var3='Python'))
 
-    # TODO 3) Write a function definition ABOVE AND OUTSIDE this class for greater_than
-    def test_3(self):
+    def test_function_3(self):
         self.assertEqual(True, greater_than(1, 2))
 
-    # TODO 4) Write a function definition ABOVE AND OUTSIDE this class for get_random_number
-    def test_4(self):
+    def test_function_4(self):
         for i in range(100):
             random_number = get_random_number(low=0, high=100)
 
             if random_number < 0 or random_number > 100:
                 self.assertTrue(False)
 
+    def test_function_5(self):
+        self.assertEqual(False, is_vegetable('apple'))
+        self.assertEqual(True,  is_vegetable('celery'))
+        self.assertEqual(False, is_vegetable('tomato'))
+        self.assertEqual(False, is_vegetable('mushroom'))
+        self.assertEqual(False, is_vegetable())
+
+    def test_function_6(self):
+        self.assertEqual('8 am', make_appointment(preferred_time_of_day='morning'))
+        self.assertEqual('1 pm', make_appointment('afternoon'))
+        self.assertEqual('5 pm', make_appointment('evening'))
+        self.assertEqual('8 am', make_appointment())
+        self.assertEqual('error', make_appointment('graveyard'))
 
 if __name__ == '__main__':
     unittest.main()
