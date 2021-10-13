4c5995fc2c12e86a00e75feb1febc4124f82fd5c
Daniel Commins <dencee@users.noreply.github.com>
2021-10-13 15:40:15 -0700

    putting back the solutions from L1M3 I accidentally deleted

--- Level1-Module3/_02_gui_app_bindings/_c_PigLatinTranslator/pig_latin_translator.py
+++ Level1-Module3/_02_gui_app_bindings/_c_PigLatinTranslator/pig_latin_translator.py
@@ -9,28 +9,40 @@ class PigLatinTranslator(tk.Tk):
         super().__init__()
 
         # TODO: Declare and initialize an Entry (tk.Entry) for the input text
+        self.input_entry = tk.Entry(self, font=('arial', 10, 'normal'), relief='solid')
 
         # TODO: Declare and initialize a Button (tk.Button) that will translate
         #  the input text to pig latin when pressed
+        self.translate_button = tk.Button(self, text='Translate', bg='light green', fg='black',
+                                          font=('arial', 12, 'normal'), relief='solid')
 
         # TODO: Declare and initialize an label (tk.Label) for the translated
         #  text
+        self.translate_label = tk.Label(self, font=('arial', 10, 'normal'), relief='solid')
 
         # TODO: Look at the example image () and place all the
         #  components in the same order
+        self.input_entry.place(relx=0.01, rely=0.01, relwidth=0.44, relheight=0.90)
+        self.translate_button.place(relx=0.45, rely=0.01, relwidth=0.1, relheight=0.90)
+        self.translate_label.place(relx=0.55, rely=0.01, relwidth=0.44, relheight=0.90)
 
         # TODO: Call the label's bind() method to call the on_key_release()
         #  method when a key is released
-
+        self.translate_button.bind('<ButtonPress>', self.on_key_press)
 
     def on_key_press(self, event):
         print('button pressed!')
 
         # TODO: Use the _c_PigLatinTranslator.translate() method to translate
         #  the text in the input entry and set the text in the output entry
-
+        translated_text = PigLatinConverter.translate(self.input_entry.get())
+        print(translated_text)
+        self.translate_label.config(text=translated_text)
 
 if __name__ == '__main__':
-    pass
     # TODO: Create a new _c_PigLatinTranslator object and set the title and geometry.
     #  Remember to call mainloop() at the end
+    game = PigLatinTranslator()
+    game.title('Pig Latin Translator')
+    game.geometry('800x50')
+    game.mainloop()
--- Level1-Module3/_02_gui_app_bindings/_e_Jeopardy/jeopardy.py
+++ Level1-Module3/_02_gui_app_bindings/_e_Jeopardy/jeopardy.py
@@ -14,8 +14,10 @@ class Jeopardy(tk.Tk):
         button_width, button_height, num_buttons = self.setup_buttons(categories)
 
         # TODO: Create a member variable for the list of categories
+        self.categories = categories
 
         # TODO: Create a member variable for the score/money
+        self.score = 0
 
         for i in range(num_buttons):
             row_num = int(i / len(categories))
@@ -27,32 +29,39 @@ class Jeopardy(tk.Tk):
             # Create the category header and buttons where
             # row 0 is the category title
             if row_num == 0:
-                pass
+
                 # TODO: To get the category name, use the categories member variable and column num
+                category_title = category.name
+                label = tk.Label(self, text=category_title, font=('Arial', 18, 'bold'), relief='solid')
 
                 # TODO: Place the Label using the 'col_x', 'row_y', 'button_width',
                 #  and 'button_height' variables
+                label.place(x=col_x, y=row_y, width=button_width, height=button_height)
 
             elif len(category.questions) > row_num - 1:
                 value = category.questions[row_num - 1].value
 
                 # TODO: Create a tk.Button with the questions' value on the button
+                button = tk.Button(self, text=value, fg='blue', font=('courier new', 24, 'bold'), relief='raised')
 
                 # TODO: Place the Button using the 'col_x', 'row_y', 'button_width',
                 #  and 'button_height' variables
+                button.place(x=col_x, y=row_y, width=button_width, height=button_height)
 
                 # TODO: Call the button's bind() method so the
                 #  on_button_press() method is called when a mouse button is pressed
                 #  example: self.joke_button.bind('<ButtonPress>', self.on_button_press)
+                button.bind('<ButtonPress>', self.on_button_press)
 
                 # TODO: Add the button to the category's list of buttons
-
+                category.buttons.append(button)
 
     def on_button_press(self, event):
         button_pressed = event.widget
         print('button ' + repr(button_pressed) + ' clicked!')
 
         # TODO: Call the ask_question() method with button_pressed as an input
+        self.ask_question(button_pressed)
 
     def ask_question(self, button_pressed):
         for category in self.categories:
@@ -69,7 +78,20 @@ class Jeopardy(tk.Tk):
                         #  the question and get their response. If their response is correct,
                         #  increase the score member variable by the value. Otherwise, subtract
                         #  value from the score
+                        response = simpledialog.askstring(category.name, question)
 
+                        if response.lower() in answer.lower():
+                            self.score += value
+                            messagebox.showinfo('Score', 'Correct, your score is: ' + str(self.score))
+                        else:
+                            self.score -= value
+                            incorrect_msg = 'Incorrect, the answer is: ' + str(answer) + '\n'
+                            incorrect_msg += 'Your score is: ' + str(self.score)
+                            messagebox.showinfo('Score', incorrect_msg)
+
+                        button.configure(text='')
+
+                        return
 
     def setup_buttons(self, categories):
         # Window size needs to be updated immediately here so the
@@ -117,9 +139,33 @@ if __name__ == '__main__':
 
     # TODO: Use the Category class above to create at least 3 question categories
     #  for your _e_Jeopardy game
+    category_1 = Category('Video Games')
 
     # TODO: For each Category, use the add_question method to add a question, answer, and
     #  a value for each question
+    category_1.add_question('Which came first: Super Mario Bros or Pac Man?', 'Pac Man', 100)
+    category_1.add_question('', '', 200)
+    category_1.add_question('', '', 300)
+
+    category_2 = Category('History')
+    category_2.add_question('WWI started in what year?', '1914', 100)
+    category_2.add_question('Greek philosopher', 'Socrates', 200)
+    category_2.add_question('', '', 300)
+
+    category_3 = Category('Science')
+    category_3.add_question('1', 'blue', 100)
+    category_3.add_question('2', 'blue', 200)
+    category_3.add_question('3', 'blue', 300)
+
+    category_4 = Category('Math')
+    category_4.add_question('foo', 'bar', 100)
+    category_4.add_question('foo', 'bar', 200)
+    category_4.add_question('foo', 'bar', 300)
+
+    j_categories.append(category_1)
+    j_categories.append(category_2)
+    j_categories.append(category_3)
+    j_categories.append(category_4)
 
     game = Jeopardy(j_categories)
     game.title('_e_Jeopardy')
