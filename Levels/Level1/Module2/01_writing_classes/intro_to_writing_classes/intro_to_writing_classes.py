b0fe4df9e487a52b57b857a3f5c2b5eed58a9bd8
Eric Busboom <eric@civicknowledge.com>
2024-02-05 14:13:56 -0800

    .

--- Levels/Level1/Module2/01_writing_classes/intro_to_writing_classes/intro_to_writing_classes.py
+++ Levels/Level1/Module2/01_writing_classes/intro_to_writing_classes/intro_to_writing_classes.py
@@ -21,47 +21,47 @@ class Student:
 
 # ================== DO NOT MODIFY THE CODE BELOW ============================
 
-class WriteClassesTests(unittest.TestCase):
+class TestWriteClassesTests(unittest.TestCase):
     student_1 = Student(name='Zeeshan', grade=7)
     student_2 = Student(name='Amelia', grade=8)
     student_3 = Student(name='Penelope', grade=9)
 
     def test_student_objects_created(self):
-        self.assertIsNotNone(WriteClassesTests.student_1, msg='student 1 not created!')
-        self.assertIsNotNone(WriteClassesTests.student_2, msg='student 2 not created!')
-        self.assertIsNotNone(WriteClassesTests.student_3, msg='student 3 not created!')
+        self.assertIsNotNone(TestWriteClassesTests.student_1, msg='student 1 not created!')
+        self.assertIsNotNone(TestWriteClassesTests.student_2, msg='student 2 not created!')
+        self.assertIsNotNone(TestWriteClassesTests.student_3, msg='student 3 not created!')
 
     def test_names(self):
-        self.assertTrue(WriteClassesTests.student_1.name == 'Zeeshan')
-        self.assertTrue(WriteClassesTests.student_2.name == 'Amelia')
-        self.assertTrue(WriteClassesTests.student_3.name == 'Penelope')
+        self.assertTrue(TestWriteClassesTests.student_1.name == 'Zeeshan')
+        self.assertTrue(TestWriteClassesTests.student_2.name == 'Amelia')
+        self.assertTrue(TestWriteClassesTests.student_3.name == 'Penelope')
 
     def test_grades(self):
-        self.assertTrue(WriteClassesTests.student_1.grade == 7)
-        self.assertTrue(WriteClassesTests.student_2.grade == 8)
-        self.assertTrue(WriteClassesTests.student_3.grade == 9)
+        self.assertTrue(TestWriteClassesTests.student_1.grade == 7)
+        self.assertTrue(TestWriteClassesTests.student_2.grade == 8)
+        self.assertTrue(TestWriteClassesTests.student_3.grade == 9)
 
     def test_student_methods(self):
-        self.assertEquals(False, WriteClassesTests.student_1.homework_done)
-        self.assertEquals(False, WriteClassesTests.student_2.homework_done)
-        self.assertEquals(False, WriteClassesTests.student_3.homework_done)
+        self.assertEquals(False, TestWriteClassesTests.student_1.homework_done)
+        self.assertEquals(False, TestWriteClassesTests.student_2.homework_done)
+        self.assertEquals(False, TestWriteClassesTests.student_3.homework_done)
 
-        WriteClassesTests.student_1.do_homework()
-        WriteClassesTests.student_2.do_homework()
-        WriteClassesTests.student_3.do_homework()
+        TestWriteClassesTests.student_1.do_homework()
+        TestWriteClassesTests.student_2.do_homework()
+        TestWriteClassesTests.student_3.do_homework()
 
-        self.assertEquals(True, WriteClassesTests.student_1.homework_done)
-        self.assertEquals(True, WriteClassesTests.student_2.homework_done)
-        self.assertEquals(True, WriteClassesTests.student_3.homework_done)
+        self.assertEquals(True, TestWriteClassesTests.student_1.homework_done)
+        self.assertEquals(True, TestWriteClassesTests.student_2.homework_done)
+        self.assertEquals(True, TestWriteClassesTests.student_3.homework_done)
 
     def test_going_to_school(self):
-        leave_str = WriteClassesTests.student_1.go_to_school(start='6 am')
+        leave_str = TestWriteClassesTests.student_1.go_to_school(start='6 am')
         self.assertEqual('Zeeshan is leaving for school at 6 am', leave_str)
 
-        leave_str = WriteClassesTests.student_2.go_to_school(start='6:30 am')
+        leave_str = TestWriteClassesTests.student_2.go_to_school(start='6:30 am')
         self.assertEqual('Amelia is leaving for school at 6:30 am', leave_str)
 
-        leave_str = WriteClassesTests.student_3.go_to_school()
+        leave_str = TestWriteClassesTests.student_3.go_to_school()
         self.assertEqual('Penelope is leaving for school at 7 am', leave_str)
 
 
