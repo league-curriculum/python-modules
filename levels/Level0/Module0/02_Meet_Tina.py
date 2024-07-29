20d6637a65893d4daceb8aa2d24ab0c2e08123b5
Eric Busboom <eric@civicknowledge.com>
2024-07-28 22:17:33 -0700

    .

--- levels/Level0/Module0/02_Meet_Tina.py
+++ levels/Level0/Module0/02_Meet_Tina.py
@@ -4,42 +4,44 @@
 # Read the program and try to understand what each line does.
 
 import turtle                           # Tell Python we want to work with the turtle
-turtle.setup (width=600, height=600)    # Set the size of the window
+turtle.setup(width=600, height=600)     # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
 
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
-tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
-forward = 150                           # Set the forward distance to 150
-left = 90                               # Set the left turn to 90 degrees
+tina.speed(2)                           # Make the turtle move as fast, but not too fast.
 
 tina.pencolor('blue')                   # Set the pen color to blue
-tina.forward(forward)                   # Move tina forward by the forward distance
-tina.left(left)                         # Turn tina left by the left turn
+tina.forward(150)                       # Move tina forward by the forward distance
+tina.left(90)                           # Turn tina left by the left turn
 
 tina.pencolor('red')                    # Set the pen color to red
-tina.forward(forward)                   # Continuie the last two steps three more times
-tina.left(left)                         # to draw a square
+tina.forward(150)                       # Continuie the last two steps three more times
+tina.left(90)                           # to draw a square
 
 tina.pencolor('green')                  # Set the pen color to green
-tina.forward(forward)
-tina.left(left)
+tina.forward(150)
+tina.left(90)
 
 tina.pencolor('purple')                 # Set the pen color to purple
-tina.forward(forward)
-tina.left(left)
+tina.forward(150)
+tina.left(90)
 
 tina.penup()                            # Lift the pen up so we can move tina without drawing
 tina.forward(20)                        # Move tina forward by 20
-tina.left(left)                         # Turn tina left by 90 degrees
+tina.left(90)                           # Turn tina left by 90 degrees
 tina.forward(20)                        # Move tina forward by 20
 tina.write("Why, hello there!")         # Write the message "Why, hello there!"
 tina.backward(20)                       # Move tina backward by 20
 
 tina.pendown()
 tina.color('red')                       # Set the color of tina to red
-tina.begin_fill() 
+tina.begin_fill()
 tina.circle(100, steps=50)
-tina.end_fill() 
+tina.end_fill()
 
 turtle.exitonclick()                    # Close the window when we click on it
+
+# Now you can try writing your own programs. Open
+# the next file 03_Turtle_Tricks.py
+
